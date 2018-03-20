bc2468eaa1f58e5be00a8f3f87d0676f10563c55
vana3 <vana3@PC538.SPSEJECNANET.CZ>
2018-03-20 11:05:24 +0100

    tredt

--- Project - dvojice/src/Main.java
+++ Project - dvojice/src/Main.java
@@ -1,9 +1,12 @@
 import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -37,7 +40,19 @@ class frame extends JFrame {
 		JButton button1 = new JButton("Sifrovany");
 		button1.addActionListener(new ActionListener() {
 		     public void actionPerformed(ActionEvent e) {
-		    	 
+		    	 try {
+		    	 FileReader fr;
+		    	 fr = new FileReader(vstup.getText());
+		    	 int c;
+		    	 String text = "";
+		    	 while((c= fr.read())!=-1) {
+		    		text = text + (char)c;
+		    	 }
+		    	 fr.close();
+		    	 } catch(Exception k) {
+		 			System.out.println("Problem");
+		 	
+		 		}
 		     }
 		});
 	
